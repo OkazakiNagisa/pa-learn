22213c66f6bc9234e3fa66b6ef322c8df89552cf
nakido <nakidosama@outlook.com>
2024-07-15 10:22:47 +0800

    layout: add clang-format

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -20,29 +20,29 @@
  */
 #include <regex.h>
 
-enum {
-  TK_NOTYPE = 256,
-  TK_EQ = '=',
+enum
+{
+    TK_NOTYPE = 256,
+    TK_EQ = '=',
 
-  /* TODO: Add more token types */
-  TK_ADD = '+',
-  TK_MINUS = '-',
-  TK_MULTIPLY = '*',
-  TK_DIV = '/',
-  TK_BRACKET_LEFT = '(',
-  TK_BRACKET_RIGHT = ')',
-  TK_NUMBER_DEC = '0'
+    /* TODO: Add more token types */
+    TK_ADD = '+',
+    TK_MINUS = '-',
+    TK_MULTIPLY = '*',
+    TK_DIV = '/',
+    TK_BRACKET_LEFT = '(',
+    TK_BRACKET_RIGHT = ')',
+    TK_NUMBER_DEC = '0'
 };
 
-static struct rule {
-  const char *regex;
-  int token_type;
+static struct rule
+{
+    const char *regex;
+    int token_type;
 } rules[] = {
-
     /* TODO: Add more rules.
      * Pay attention to the precedence level of different rules.
      */
-
     {" +", TK_NOTYPE}, // spaces
     {"\\+", '+'},      // plus
     {"==", TK_EQ},     // equal
@@ -61,139 +61,170 @@ static regex_t re[NR_REGEX] = {};
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
-void init_regex() {
-  int i;
-  char error_msg[128];
-  int ret;
+void init_regex()
+{
+    int i;
+    char error_msg[128];
+    int ret;
 
-  for (i = 0; i < NR_REGEX; i++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
-      regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    for (i = 0; i < NR_REGEX; i++)
+    {
+        ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+        if (ret != 0)
+        {
+            regerror(ret, &re[i], error_msg, 128);
+            panic("regex compilation failed: %s\n%s", error_msg,
+                  rules[i].regex);
+        }
     }
-  }
 }
 
-typedef struct token {
-  int type;
-  char str[32];
+typedef struct token
+{
+    int type;
+    char str[32];
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used)) = 0;
 
-static bool make_token(char *e) {
-  int position = 0;
-  int i;
-  regmatch_t pmatch;
+static bool make_token(char *e)
+{
+    int position = 0;
+    int i;
+    regmatch_t pmatch;
 
-  nr_token = -1;
+    nr_token = -1;
 
-  while (e[position] != '\0') {
-    /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 &&
-          pmatch.rm_so == 0) {
-        char *substr_start = e + position;
-        int substr_len = pmatch.rm_eo;
+    while (e[position] != '\0')
+    {
+        /* Try all rules one by one. */
+        for (i = 0; i < NR_REGEX; i++)
+        {
+            if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 &&
+                pmatch.rm_so == 0)
+            {
+                char *substr_start = e + position;
+                int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i,
-            rules[i].regex, position, substr_len, substr_len, substr_start);
+                Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+                    i, rules[i].regex, position, substr_len, substr_len,
+                    substr_start);
 
-        position += substr_len;
+                position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
-        if (nr_token >= 31) {
-          Log("Token count too long!");
-          break;
+                /* TODO: Now a new token is recognized with rules[i]. Add codes
+                 * to record the token in the array `tokens'. For certain types
+                 * of tokens, some extra actions should be performed.
+                 */
+                if (nr_token >= 31)
+                {
+                    Log("Token count too long!");
+                    break;
+                }
+                nr_token++;
+                tokens[nr_token].type = rules[i].token_type;
+
+                switch (rules[i].token_type)
+                {
+                case TK_NUMBER_DEC:
+                    if (substr_len >= 32)
+                    {
+                        Log("Token length too long!");
+                        break;
+                    }
+                    memcpy(&tokens[nr_token].str, &e[position - substr_len],
+                           substr_len);
+                    tokens[nr_token].str[substr_len] = 0;
+                    break;
+                case TK_EQ:
+                case TK_ADD:
+                case TK_MINUS:
+                case TK_MULTIPLY:
+                case TK_DIV:
+                case TK_BRACKET_LEFT:
+                case TK_BRACKET_RIGHT:
+                case TK_NOTYPE:
+                    break;
+                default:
+                    panic("???");
+                }
+
+                break;
+            }
         }
-        nr_token++;
-        tokens[nr_token].type = rules[i].token_type;
 
-        switch (rules[i].token_type) {
-        case TK_NUMBER_DEC:
-          if (substr_len >= 32) {
-            Log("Token length too long!");
-            break;
-          }
-          memcpy(&tokens[nr_token].str, &e[position - substr_len], substr_len);
-          tokens[nr_token].str[substr_len] = 0;
-          break;
-        case TK_EQ:
-        case TK_ADD:
-        case TK_MINUS:
-        case TK_MULTIPLY:
-        case TK_DIV:
-        case TK_BRACKET_LEFT:
-        case TK_BRACKET_RIGHT:
-        case TK_NOTYPE:
-          break;
-        default:
-          panic("???");
+        if (i == NR_REGEX)
+        {
+            printf("no match at position %d\n%s\n%*.s^\n", position, e,
+                   position, "");
+            return false;
         }
-
-        break;
-      }
     }
 
-    if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-      return false;
-    }
-  }
-
-  return true;
+    return true;
 }
 
-typedef struct IntResult {
-  int result;
-  bool succeeded;
+typedef struct IntResult
+{
+    int result;
+    bool succeeded;
 } IntResult;
 
-bool check_parenteses(int p, int q) {
-  if (tokens[p].type != TK_BRACKET_LEFT || tokens[q].type != TK_BRACKET_RIGHT) {
-    return false;
-  }
-  int stack_pointer = 0;
-  for (int i = p + 1; p <= q - 1; p++) {
-    if (tokens[i].type == TK_BRACKET_LEFT) {
-      stack_pointer++;
-    }
-    if (tokens[i].type == TK_BRACKET_RIGHT) {
-      if (--stack_pointer < 0)
+bool check_parenteses(int p, int q)
+{
+    if (tokens[p].type != TK_BRACKET_LEFT || tokens[q].type != TK_BRACKET_RIGHT)
         return false;
+
+    int stack_pointer = 0;
+    for (int i = p + 1; p <= q - 1; p++)
+    {
+        if (tokens[i].type == TK_BRACKET_LEFT)
+            stack_pointer++;
+
+        if (tokens[i].type == TK_BRACKET_RIGHT)
+            if (--stack_pointer < 0)
+                return false;
+
     }
-  }
-  return stack_pointer == 0;
+    return stack_pointer == 0;
 }
 
-IntResult eval(int p, int q) {
-  IntResult ret;
-  ret.succeeded = true;
-  if (p > q || p > nr_token || q > nr_token) {
-    panic("????");
-  } else if (p == q) {
-    ret.result = atoi(tokens[p].str);
-    return ret;
-  } else if (check_parenteses(p, q)) {
-    return eval(p + 1, q - 1);
-  } else {
-    
-  }
+IntResult eval(int p, int q)
+{
+    IntResult ret;
+    ret.succeeded = true;
+    if (p > q || p > nr_token || q > nr_token)
+        panic("????");
+
+    else if (p == q)
+    {
+        ret.result = atoi(tokens[p].str);
+        return ret;
+    }
+    else if (check_parenteses(p, q))
+        return eval(p + 1, q - 1);
+    else
+    {
+        int split_pos = p;
+        for (int i = p; p <= q; p++)
+        {
+            switch (tokens[i].type) {
+            }
+        }
+    }
 }
 
-word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
+word_t expr(char *e, bool *success)
+{
+    if (!make_token(e))
+    {
+        *success = false;
+        return 0;
+    }
+
+    /* TODO: Insert codes to evaluate the expression. */
+    // TODO();
+
     return 0;
-  }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  // TODO();
-
-  return 0;
 }
